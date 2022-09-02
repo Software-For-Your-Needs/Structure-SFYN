34654a9fe81831531b2c6f3bbc02c1c454485424
Jenny Cabrejo Marin <jennycabrejomarin@gmail.com>
2022-09-02 09:52:05 -0500

    Se agrga la tabla para la case MovimientoDinero y se completan los servicios para esta misma

--- StructureSFYN/src/main/java/com/example/StructureSFYN/services/MovimientoDineroService.java
+++ StructureSFYN/src/main/java/com/example/StructureSFYN/services/MovimientoDineroService.java
@@ -40,19 +40,54 @@ public class MovimientoDineroService {
     }
 
     @Override
-    public MovimientoDinero editarMovimientoDinero(MovimientoDinero editarMovimientoDinero){
-        Empresa empresaBD = repository.findById(id).get();
+    public MovimientoDinero editarMovimientoDinero(MovimientoDinero movimientoDinero, Long id){
+        MovimientoDinero movimientoDineroBD = movimientoDineroRepository.findById(id).get();
 
         if (Objects.nonNull(
-                empresa.getNombreEmpresa())
+                movimientoDineroBD.getMontoMovimiento())
                 && !"".equalsIgnoreCase(
-                empresa.getNombreEmpresa())) {
-            empresaBD.setNombreEmpresa(
-                    empresa.getNombreEmpresa());
+                movimientoDinero.getMontoMovimiento().toString())) {
+            movimientoDineroBD.setMontoMovimiento(
+                    movimientoDinero.getMontoMovimiento());
+        }
+
+        if (Objects.nonNull(
+                movimientoDineroBD.getMontosPositivos())
+                && !"".equalsIgnoreCase(
+                movimientoDinero.getMontosPositivos().toString())) {
+            movimientoDineroBD.setMontosPositivos(
+                    movimientoDinero.getMontosPositivos());
+
+        }
+
+        if (Objects.nonNull(
+                movimientoDineroBD.getMontosNegativos())
+                && !"".equalsIgnoreCase(
+                movimientoDinero.getMontosNegativos().toString())) {
+            movimientoDineroBD.setMontosNegativos(
+                    movimientoDinero.getMontosNegativos());
+
+        }
+
+        if (Objects.nonNull(
+                movimientoDineroBD.getConceptoMovimiento())
+                && !"".equalsIgnoreCase(
+                movimientoDinero.getConceptoMovimiento())) {
+            movimientoDineroBD.setConceptoMovimiento(
+                    movimientoDinero.getConceptoMovimiento());
+        }
+
+        if (Objects.nonNull(
+                movimientoDineroBD.getUsuarioContabilidad())
+                && !"".equalsIgnoreCase(
+                movimientoDinero.getUsuarioContabilidad())) {
+            movimientoDineroBD.setUsuarioContabilidad(
+                    movimientoDinero.getUsuarioContabilidad());
         }
         
+        return this.movimientoDineroRepository.save(movimientoDineroBD);
+                          
     }
-
 
     @Override
     public void eliminarMovimientoDinero(Long id){
