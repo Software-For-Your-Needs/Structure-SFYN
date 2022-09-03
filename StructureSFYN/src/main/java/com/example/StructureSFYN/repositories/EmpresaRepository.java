ebda2e488fc398530e147eb578682f0e20479137
carivantelva15 <carivantelva@gmail.com>
2022-09-02 21:16:02 -0500

    se crean servicios y controladores

--- StructureSFYN/src/main/java/com/example/StructureSFYN/repositories/EmpresaRepository.java
+++ StructureSFYN/src/main/java/com/example/StructureSFYN/repositories/EmpresaRepository.java
@@ -1,8 +1,11 @@
 package com.example.StructureSFYN.repositories;
 
+import com.example.StructureSFYN.entities.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface EmpresaRepository extends JpaRepository <Empresa, Long> {
+public interface EmpresaRepository extends JpaRepository <Empresa, Integer> {
 
 }
--- StructureSFYN/src/main/java/com/example/StructureSFYN/services/EmpresaService.java
+++ StructureSFYN/src/main/java/com/example/StructureSFYN/services/EmpresaService.java
@@ -2,6 +2,7 @@ package com.example.StructureSFYN.services;
 
 import com.example.StructureSFYN.entities.Empresa;
 import com.example.StructureSFYN.repositories.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,27 +22,27 @@ public class EmpresaService {
     }
 
     //
-    @Override
+   // @Override
     public List<Empresa>getListaEmpresa(){
         return this.empresaRepository.findAll();
     }
-    @Override
-    public Optional<Empresa>getEmpresa(Long id){
+   // @Override
+    public Optional<Empresa>getEmpresa(int id){
         return this.empresaRepository.findById(id);
     }
-    @Override
+   // @Override
     public Empresa crearEmpresa(Empresa newEmpresa){
         return  this.empresaRepository.save(newEmpresa);
     }
 
-    @Override
-    public void eliminarEmpresa(Long id){
-      this.empresaRepository.deleteById();
+    //@Override
+    public void eliminarEmpresa(int id){
+      this.empresaRepository.deleteById(id);
     }
 
-    @Override
-    public Empresa editarEmpresa(Empresa empresa, Long id){
-        Empresa empresaBD = repository.findById(id).get();
+   // @Override
+    public Empresa editarEmpresa(Empresa empresa, int id) {
+        Empresa empresaBD = empresaRepository.findById(id).get();
 
         if (Objects.nonNull(
                 empresa.getNombreEmpresa())
@@ -50,6 +51,50 @@ public class EmpresaService {
             empresaBD.setNombreEmpresa(
                     empresa.getNombreEmpresa());
         }
+        //*************************************
+        if (Objects.nonNull(
+                empresa.getCiudadEmpresa())
+                && !"".equalsIgnoreCase(
+                empresa.getCiudadEmpresa())) {
+            empresaBD.setCiudadEmpresa(
+                    empresa.getCiudadEmpresa());
+        }
+        if (Objects.nonNull(
+                empresa.getDireccionEmpresa())
+                && !"".equalsIgnoreCase(
+                empresa.getDireccionEmpresa())) {
+            empresaBD.setDireccionEmpresa(
+                    empresa.getDireccionEmpresa());
+        }
+        if (Objects.nonNull(
+                empresa.getNitEmpresa())
+                && !"".equalsIgnoreCase(
+                empresa.getNitEmpresa().toString())) {
+            empresaBD.setNitEmpresa(
+                    empresa.getNitEmpresa());
+        }
+        if (Objects.nonNull(
+                empresa.getCorreoEmpresa())
+                && !"".equalsIgnoreCase(
+                empresa.getCorreoEmpresa())) {
+            empresaBD.setCorreoEmpresa(
+                    empresa.getCorreoEmpresa());
+        }
+        if (Objects.nonNull(
+                empresa.getTipoEmpresa())
+                && !"".equalsIgnoreCase(
+                empresa.getTipoEmpresa())) {
+            empresaBD.setTipoEmpresa(
+                    empresa.getTipoEmpresa());
+        }
+        if (Objects.nonNull(
+                empresa.getTelefonoEmpresa())
+                && !"".equalsIgnoreCase(
+                empresa.getTelefonoEmpresa())) {
+            empresaBD.setTelefonoEmpresa(
+                    empresa.getTelefonoEmpresa());
+        }
+        return empresaRepository.save(empresaBD);
     }
 
 }
